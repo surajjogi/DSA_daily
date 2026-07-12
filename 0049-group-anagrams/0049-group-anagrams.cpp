05afb38b599ce125416552faa4dae37519f1acd4
suarj9799 <151529928+surajjogi@users.noreply.github.com>
2026-07-12 14:58:34 +0530

    Time: 96 ms (5.14%), Space: 52 MB (5.25%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-// trick
-//mp["asd"].push_back("one");
-// mp["asd"].push_back("two");
-
-// Now the map contains:
-
-// "asd" -> {"one", "two"}
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string,vector<string>>mp;
-        for(int i=0;i<strs.size();i++){
-            string ss=strs[i];
-            sort(ss.begin(),ss.end());
-            mp[ss].push_back(strs[i]);
+        map<map<char, int>, vector<string>> mp;
+      
+        for (int i = 0; i < strs.size(); i++) {
+              map<char, int> freq;
+            string s = strs[i];
+            for (int j = 0; j < s.size(); j++) {
+                freq[s[j]]++;
+            }
+            mp[freq].push_back(strs[i]);
         }
-        vector<vector<string>>v;
-        for(auto it:mp){
-            v.push_back(it.second);
+        vector<vector<string>> ans;
+        for (auto it : mp) {
+            ans.push_back(it.second);
         }
-
-return v;
-
+        return ans;
     }
 };
