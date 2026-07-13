30019738ba875e77035cdbaba4d7d9defd9f02ab
suarj9799 <151529928+surajjogi@users.noreply.github.com>
2026-07-13 14:26:55 +0530

    Time: 0 ms (100%), Space: 10.7 MB (51.64%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-     int maxcost(vector<int>&dp,vector<int>&nums,int n){
-        if(n==0){
-            return nums[0];
-
-        }
-        if(n==1){
-            return max(nums[0],nums[1]);
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        return dp[n]=max(nums[n]+maxcost(dp,nums,n-2),maxcost(dp,nums,n-1));
-     }
-
-    
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1){
-            return nums[0];
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        if(n== 2) return max(nums[0],nums[1]);
+        vector<int> dp1(n-1); // remove i = 0
+        vector<int> dp2(n-1);// remove i = n-1;
+
+        dp1[0] = nums[1];
+        dp1[1] = max(nums[1], nums[2]);
+
+        dp2[0] = nums[0];
+        dp2[1] = max(nums[0], nums[1]);
+
+        for(int i = 2; i<n-1; i++){
+            dp1[i] = max(nums[i+1] + dp1[i - 2], dp1[i - 1]);
+            dp2[i] = max(nums[i] + dp2[i - 2], dp2[i - 1]);
         }
-      vector<int> dp1(n - 1, -1);
-vector<int> dp2(n - 1, -1);
-        vector<int> withoutFirst(nums.begin() + 1, nums.end());
-        int n1=withoutFirst.size();
-        vector<int> withoutLast(nums.begin(), nums.end() - 1);
-        int n2=withoutLast.size();
-        int max1=maxcost(dp1,withoutFirst,n1-1);
-        int max2=maxcost(dp2,withoutLast,n1-1);
-        if(max1>max2){
-            return max1;
-        }
-        return max2;
+        return max(dp1[n-2], dp2[n-2]);
     }
 };
