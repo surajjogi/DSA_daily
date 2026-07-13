dc48009e402f5623ec0e00fa31d96324519efbda
suarj9799 <151529928+surajjogi@users.noreply.github.com>
2026-07-13 12:09:46 +0530

    Time: 0 ms (100%), Space: 11 MB (18.77%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+     int maxcost(vector<int>&dp,vector<int>&nums,int n){
+        if(n==0){
+            return nums[0];
+
+        }
+        if(n==1){
+            return max(nums[0],nums[1]);
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=max(nums[n]+maxcost(dp,nums,n-2),maxcost(dp,nums,n-1));
+     }
+
+    
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1){
+            return nums[0];
+        }
+      vector<int> dp1(n - 1, -1);
+vector<int> dp2(n - 1, -1);
+        vector<int> withoutFirst(nums.begin() + 1, nums.end());
+        vector<int> withoutLast(nums.begin(), nums.end() - 1);
+        int max1=maxcost(dp1,withoutFirst,n-2);
+        int max2=maxcost(dp2,withoutLast,n-2);
+        if(max1>max2){
+            return max1;
+        }
+        return max2;
+    }
+};
