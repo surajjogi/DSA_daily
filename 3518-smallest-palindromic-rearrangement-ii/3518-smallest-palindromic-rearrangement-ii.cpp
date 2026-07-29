d5d522a38096553f4659c6642f93eb68b0854271
suarj9799 <151529928+surajjogi@users.noreply.github.com>
2026-07-30 04:04:23 +0530

    Time: 46 ms (38.17%), Space: 17.2 MB (57.25%) - LeetHub

--- 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
@@ -1,82 +1,87 @@
-constexpr int INF=1e6+1, N=24;
-int C[N][N]={{0}};
-
-constexpr void Pascal() {
-    if (C[0][0]==1) return; // computed once
-    C[0][0]=1;
-    for (int i=1; i<N; i++) {
-        C[i][0]=C[i][i]=1;
-        for (int j=1; j<=i/2; j++) {
-            C[i][j]=C[i][i-j]=C[i-1][j-1]+C[i-1][j];
-        }
-    }
-}
-
 class Solution {
 public:
-    static int comb(int n, int k) {
-        if (n<N) return C[n][k];//  look at the table
-        if (2*k>n) k=n-k;// C(n, n-k)=C(n, k)
-        int64_t ans=1;
-        for (int i=1; i<=k; i++) {
-            ans=ans*(n-i+1)/i;
-            if (ans>=INF) return INF;
+  static const long long LIMIT = 1000001;
+
+    long long nCr(int n, int r) {
+        if (r > n) return 0;
+        r = min(r, n - r);
+
+        long long ans = 1;
+        for (int i = 1; i <= r; i++) {
+            ans = ans * (n - i + 1) / i;
+            if (ans >= LIMIT) return LIMIT;
         }
         return ans;
     }
 
-    static int perm(const array<int, 26>& freq, int sz) {
-        int64_t ans=1;
-        for (int f : freq) {
-            if (f==0) continue;
-            ans*=comb(sz, f);
-            if (ans>=INF) return INF;
-            sz-=f;
+
+    long long  countways(vector<int>&nums){
+        int total = 0 ;
+        for(auto it : nums){
+            total +=it;
+        }
+        long long ans = 1;
+        int rem = total;
+        for(int x : nums){
+            if(x ==0) continue;
+            ans *= nCr(rem , x);
+            if(ans >= LIMIT){
+                ans = LIMIT;
+            }
+            rem -=x;
         }
         return ans;
+
     }
 
-    static string smallestPalindrome(string& s, int k) {
-        Pascal();
-        const int n=s.size(), n0=n/2;
-        array<int, 26> freq={0};
-        for (int i=0; i<n0; i++)
-            freq[s[i]-'a']++;
-        
-        int total=perm(freq, n0);
-        if (k>total) return "";
 
-        string left;
-        left.reserve(n);
-        int sz=n0;
-        for (int i=0; i<n0; i++) {
-            for (int c=0; c<26; c++) {
-                if (freq[c]==0) continue;
-                freq[c]--;
-                int cnt=perm(freq, --sz);
-                if (cnt>=k) {
-                    left.push_back('a' + c);
-                    break;
-                } 
-                else {
-                    k -= cnt;
-                    freq[c]++;// backtrack
-                    sz++;
-                }
+
+    string smallestPalindrome(string s, int k) {
+        // step 1 ; count the frequency of every character in the string: 
+        vector<int> freq(26);
+        for (char c : s){
+            freq[c - 'a']++;
+        }
+        vector<int>half(26);
+        string mid = "";
+        for(int i = 0 ;i < 26 ;i++){
+            half[i] = freq[i]/2;
+            if(freq[i] %2 !=0){
+                mid.push_back(char('a' + i));
             }
         }
 
-        string right=left;
+        if(countways(half) <  k ){ // means the number of permutaions are less than the required;
+            return "";
+        }
+
+        int len = 0 ;
+        for(auto it : half){
+            len += it;
+        }
+
+        // start greedily : - 
+        string left;
+        for (int pos = 0 ; pos < len; pos++) {
+             for(int c = 0 ; c < 26 ; c++){
+                if(half[c] ==0){
+                    continue;
+                }
+                half[c]--;
+                long long  cnt = countways(half);
+                if(cnt  >= k){
+                    left += char('a' + c);
+                    break;
+                }else{
+                    k -=cnt;
+                    half[c]++; // restore it if rejected : 
+                }
+             }
+        }
+
+        string right = left;
         reverse(right.begin(), right.end());
-        if (n&1) left.push_back(s[n/2]);
-        left.append(right);
-        return left;
+        return left + mid + right;
+
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
