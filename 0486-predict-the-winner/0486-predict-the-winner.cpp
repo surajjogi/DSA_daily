478320711009bd69379d1b0e439d512de4e3c003
suarj9799 <151529928+surajjogi@users.noreply.github.com>
2026-08-01 22:12:04 +0530

    Time: 99 ms (14.85%), Space: 10.3 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    int maxDiff(vector<int>& nums, int left, int right, int n,
-                vector<vector<int>>& dp) {
+    int maxDiff(vector<int>& nums, int left, int right, int n) {
         if (left == right) {
             return nums[left];
         }
 
-        if (dp[left][right] != -1) {
-            return dp[left][right];
-        }
 
-        int scoreL = nums[left] - maxDiff(nums, left + 1, right, n, dp);
-        int scoreR = nums[right] - maxDiff(nums, left, right - 1, n, dp);
+        int scoreL = nums[left] - maxDiff(nums, left + 1, right, n);
+        int scoreR = nums[right] - maxDiff(nums, left, right - 1, n);
 
-        dp[left][right] = max(scoreL, scoreR);
-        return dp[left][right];
+      
+        return max(scoreL,scoreR);
     }
 
     bool predictTheWinner(vector<int>& nums) {
@@ -22,6 +18,6 @@ public:
 
         vector<vector<int>> dp(n, vector<int>(n, -1));
 
-        return maxDiff(nums, 0, n - 1, n, dp) >= 0;
+        return maxDiff(nums, 0, n - 1, n) >= 0;
     }
 };
