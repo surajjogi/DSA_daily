216b8437f1d201ea551d9da971bcf864f0053800
suarj9799 <151529928+surajjogi@users.noreply.github.com>
2026-08-03 09:09:49 +0530

    Time: 18 ms (15.79%), Space: 19.6 MB (28.59%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+ int maxDiff(vector<int>& nums, int left, int right,vector<vector<int>>& dp) {
+        if (left == right) {
+            return nums[left];
+        }
+        if(dp[left][right]!=-1)
+        {
+            return dp[left][right];
+        }
+        int scoreL = nums[left] - maxDiff(nums, left + 1, right,dp);
+        int scoreR = nums[right] - maxDiff(nums, left, right - 1,dp); 
+        return dp[left][right]= max(scoreL,scoreR);
+        }
     bool stoneGame(vector<int>& piles) {
-        return 1;
+         int n = piles.size();
+       vector<vector<int>> dp(n, vector<int>(n, -1));
+        return maxDiff(piles, 0, n - 1,dp) >= 0;
     }
 };
