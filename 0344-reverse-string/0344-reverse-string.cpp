8c349a4ad052260abcd8472566f81b6575599829
suarj9799 <151529928+surajjogi@users.noreply.github.com>
2026-08-17 17:31:57 +0530

    Time: 0 ms (100%), Space: 27.3 MB (15.98%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left=0;
+        int right=s.size()-1;
+           for(int i=0;i<s.size()/2;i++){
+            swap(s[left],s[right]);
+            left++;
+            right--;
+           }
+
+    }
+};
