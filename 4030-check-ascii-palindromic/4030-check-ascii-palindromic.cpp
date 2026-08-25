694fdf7861d5701d33664de7fe3f4166f2691efb
suarj9799 <151529928+surajjogi@users.noreply.github.com>
2026-08-25 20:59:11 +0530

    Time: 3 ms (46.55%), Space: 9.7 MB (32.44%) - LeetHub

--- 4030-check-ascii-palindromic/4030-check-ascii-palindromic.cpp
+++ 4030-check-ascii-palindromic/4030-check-ascii-palindromic.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     bool isPalindromic(string s) {
-        vector<int>v;
-        int i=0;
-        while(i<s.size()){
-            v.push_back((s[i]-'a')+97);
-            i++;
+        vector<int> v;
+
+        for (int i = 0; i < s.size(); i++) {
+            v.push_back((s[i] - 'a') + 97);
         }
-        string ss="";
-       i=0;
-       while(i<v.size()){
-        ss+=bitset<8>(v[i]).to_string();
-        i++;
-       }
-       i=0;
-       int left=0;
-       int right=ss.size()-1;
-     int num=ss.size()%2?ss.size()-1:ss.size();
-      while(left<=num/2){
-        if(ss[left]!=ss[right]){
-            return 0;
+
+        string ss = "";
+
+        for (int i = 0; i < v.size(); i++) {
+            ss += bitset<8>(v[i]).to_string();
         }
-        left++;
-        right--;
-      }
-     return 1;
+
+        int left = 0;
+        int right = ss.size() - 1;
+
+        while (left < right) {
+            if (ss[left] != ss[right]) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
     }
 };
