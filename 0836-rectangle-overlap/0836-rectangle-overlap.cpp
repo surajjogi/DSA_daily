93284c006ba836906dcbe83b54f0642f034c39db
suarj9799 <151529928+surajjogi@users.noreply.github.com>
2026-09-14 15:43:21 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.04%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& r1, vector<int>& r2) {
+        if(r1[0] < r2[2] && r2[0] < r1[2] &&
+               r1[1] < r2[3] && r2[1] < r1[3]){
+            return true;
+        }
+         return false;
+    }
+}; 
